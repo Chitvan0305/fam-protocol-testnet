3a37ca885528c9060785003bd53cc3ae8cb5a49d
Chitvan Garg <gargchitvan378@gmail.com>
2024-11-24 00:51:06 +0530

    fam-protocol-testnet

--- quest_backend/src/index.ts
+++ quest_backend/src/index.ts
@@ -48,7 +48,7 @@ app.use(cookieParser());
 // );
 
 app.use(cors({
-  origin: 'http://localhost:3000', // Allow your frontend origin
+  origin: 'https://famtest-render', // Allow your frontend origin
   credentials: true // Allow credentials
 }));
 
